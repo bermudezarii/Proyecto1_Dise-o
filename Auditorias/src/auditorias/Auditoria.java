03600bc5de1d8952588fb1c23f59e0905b96e924
Ariana B <bermudezarii@gmail.com>
2017-03-14 17:30:01 -0600

    Add files via upload

---
+++ Auditorias/src/auditorias/Auditoria.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auditorias;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Ariana
+ */
+public class Auditoria {
+    private int id; 
+    private Date fchinicio; 
+    private Date fchfinal; 
+    private Tipo tipo; 
+    private Naturaleza naturaleza; 
+    
+}
